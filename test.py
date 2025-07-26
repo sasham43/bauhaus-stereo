3e203068c2af6fb0369e3308a1ef9fbac6ea8cb8
sasham43 <sashasemail@gmail.com>
2025-07-26 11:57:06 -0400

    update python

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-from stereo import play_audio_file
-
+def play_audio_file(file):
+    try:
+        # p = subprocess.Popen(['mpv', file])
+        p = subprocess.Popen(['ffplay', '-nodisp', '-autoexit', file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        p.wait()
+        # process = subprocess.Popen(['cvlc', file])
+        # process.wait()
+    except Exception as e:
+        print(f"Error playing audio: {e}")
 
 
 play_audio_file("/home/pi/audio/80s/alanparsonsproject_sirius.opus")
